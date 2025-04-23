c9a888462dc5bc7e45890559e880db0e08d466fa
hellybrine <aaravmachhral7@gmail.com>
2025-04-23 19:34:21 +0530

    this also failed

--- agent/reward.py
+++
@@ -1,9 +0,0 @@
-class RewardFunction:
-    def __call__(self, session_dict):
-        reward = 0
-        reward += session_dict["duration_secs"] * 0.1
-        reward += session_dict["num_commands"] * 0.2
-        reward += session_dict["fake_file_access"] * 0.3
-        if session_dict["bot_detected"]:
-            reward -= 1.0
-        return reward
